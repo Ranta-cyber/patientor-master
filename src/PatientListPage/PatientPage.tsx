d03aefb3d7f615290492c558a2cffbcb4f6be701
sirpa <64065711+Ranta-cyber@users.noreply.github.com>
2021-01-03 10:27:09 +0200

    tehtävä 9.20

--- src/PatientListPage/PatientPage.tsx
+++ src/PatientListPage/PatientPage.tsx
@@ -4,7 +4,7 @@ import { useParams } from "react-router-dom";
 import { Icon } from "semantic-ui-react";
 import { PatientFormValues } from "../AddPatientModal/AddPatientForm";
 import AddPatientModal from "../AddPatientModal";
-import { Patient } from "../types";
+import { Diagnosis, Patient, BaseEntry } from "../types";
 import { apiBaseUrl } from "../constants";
 import { useStateValue } from "../state";
 
@@ -14,8 +14,6 @@ const PatientPage: React.FC = () => {
   const { id } = useParams<{ id: string }>();
   const [modalOpen, setModalOpen] = React.useState<boolean>(false);
   const [error, setError] = React.useState<string | undefined>();
-
-
 
   const openModal = (): void => setModalOpen(true);
 
@@ -28,18 +26,47 @@ const PatientPage: React.FC = () => {
 
   console.log('patient on:', patient);
 
+  const Codes = (codes: BaseEntry) => {
+    if (codes.diagnosisCodes) {
+      return (
+        codes.diagnosisCodes.map(code => (
+          <li key={code}>
+            {code}
+          </li>))
+      );
+    }
+    return <p>No entries</p>;
+  };
+
   return (
     <div>
       <h3>Patient </h3>
 
-      <h1>{patient.name} 
-      {patient.gender === 'male' ? <Icon name='mars'/> : <Icon name='venus'/>}
-      
-      </h1> 
-      
+      <h1>{patient.name}
+        {patient.gender === 'male' ? <Icon name='mars' /> : <Icon name='venus' />}
+
+      </h1>
+
       <p> SSN: {patient.ssn} </p>
       <p> Birth date: {patient.dateOfBirth} </p>
       <p> Occupation: {patient.occupation} </p>
+
+      <h2> entries</h2>
+      {patient.entries.map(patientEntry =>
+        <li key={patientEntry.id}>{patientEntry.date} {patientEntry.description}
+          {patientEntry.diagnosisCodes &&
+
+            patientEntry.diagnosisCodes.map(code => (
+              <li key={code}>
+                {code}
+              </li>))
+
+            // ( <Codes diagnosisCodes={patientEntry.diagnosisCodes} /> )
+          }
+
+
+        </li>)}
+
     </div>
 
   );
--- src/types.ts
+++ src/types.ts
@@ -10,11 +10,78 @@ export enum Gender {
   Other = "other"
 }
 
+
+
+export type Entry =
+  | HealthCheckEntry
+  | OccupationalHealthcareEntry
+  | HospitalEntry;
+
+
+export interface BaseEntry {
+  id: string;
+  description: string;
+  date: string;
+  specialist: string;
+  diagnosisCodes?: Array<Diagnosis['code']>;
+}
+
+export enum EntryType {
+  HealthCheck = "HealthCheck",
+  OccupationalHealthCare = "OccupationalHealthcare",
+  Hospital = "Hospital",
+}
+
+export enum HealthCheckRating {
+  "Healthy" = 0,
+  "LowRisk" = 1,
+  "HighRisk" = 2,
+  "CriticalRisk" = 3
+}
+
+interface HealthCheckEntry extends BaseEntry {
+  type: "HealthCheck";
+  healthCheckRating: HealthCheckRating;
+}
+
+interface HospitalEntry extends BaseEntry {
+  type: "Hospital";
+  discharge: Discharge;
+}
+
+interface OccupationalHealthcareEntry extends BaseEntry {
+  type: "OccupationalHealthcare";
+  employerName: string;
+  sickLeave?: SickLeave;
+}
+
+export interface Discharge {
+  date: string;
+  criteria: string;
+}
+
+export interface SickLeave {
+  startDate: string;
+  endDate: string;
+}
+
+export type Entries =
+  | HospitalEntry
+  | OccupationalHealthcareEntry
+  | HealthCheckEntry;
+
+export type NewBaseEntry = Omit<BaseEntry, "id">;
+
+export type PublicPatient = Omit<Patient, 'ssn' | 'entries' >;
+
+export type NewPatientEntry = Omit<Patient, 'id'>;
+
 export interface Patient {
   id: string;
   name: string;
-  occupation: string;
-  gender: Gender;
+  dateOfBirth: string;
   ssn?: string;
-  dateOfBirth?: string;
-}
+  gender: Gender;
+  occupation: string;
+  entries: Entry[];
+} 
